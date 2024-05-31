0de14bb201b2b28c6cf7a5fa26c1666b001f0eac
plugkevo <munenekevin366@gmail.com>
2024-05-31 16:27:48 +0300

    search query done

--- app/src/main/java/com/example/firebase_app/ViewGoods.kt
+++ app/src/main/java/com/example/firebase_app/ViewGoods.kt
@@ -3,6 +3,8 @@ package com.example.firebase_app
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -21,11 +24,27 @@ class ViewGoods : AppCompatActivity() {
     private lateinit var tvLoadingData: TextView
     private lateinit var empList: ArrayList<GoodsModel>
     private lateinit var dbRef: DatabaseReference
+    private lateinit var searchBar:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_goods)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, ViewGoods::class.java)
+            startActivity(intent)
+        }
+        searchBar = findViewById(R.id.searchBar)
+
+        val searchButton = findViewById<Button>(R.id.btnSearch)
+        searchButton.setOnClickListener {
+            val searchQuery = searchBar.text.toString().trim()
+            performSearch(searchQuery)
+        }
+
 
         empRecyclerView = findViewById(R.id.rvGoods)
         empRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -41,6 +60,7 @@ class ViewGoods : AppCompatActivity() {
             insets
         }
     }
+
 
     private fun getEmployeesData() {
 
@@ -86,6 +106,54 @@ class ViewGoods : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
 
+        })
+
+    }
+    private fun performSearch(query: String) {
+        // This is a basic example searching by title. You can modify based on your data structure.
+        val queryRef = dbRef.orderByChild("goodsNumber")
+            .startAt(query)
+            .endAt(query + "\uf8ff")  // Search by starting characters and include special character for wider range
+
+        queryRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Handle search results here
+                if (dataSnapshot.exists()) {
+                    val searchResults = arrayListOf<GoodsModel>()
+                    for (empSnap in dataSnapshot.children) {
+                        val empData = empSnap.getValue(GoodsModel::class.java)
+                        searchResults.add(empData!!)
+                    }
+                    val mAdapter = ViewGoodsAdapter(searchResults)
+                    empRecyclerView.adapter = mAdapter
+
+                    //when an item is clicked the all the employee data is displayed in a different page
+                    mAdapter.setOnItemClickListener(object : ViewGoodsAdapter.onItemClickListener {
+                        override fun onItemClick(position: Int) {
+                            val intent = Intent(this@ViewGoods, GoodsDetailsActivity::class.java)
+
+                            //put extras
+                            intent.putExtra("goodsId", searchResults[position].GoodsId)
+                            intent.putExtra("goodsName", searchResults[position].GoodsName)
+                            intent.putExtra("goodsNumber", searchResults[position].GoodsNumber)
+                            intent.putExtra("storeName", searchResults[position].StoreName)
+                            startActivity(intent)
+                        }
+                    })
+
+                    empRecyclerView.visibility = View.VISIBLE
+                    tvLoadingData.visibility = View.GONE
+                } else {
+                    // No data found
+                    tvLoadingData.text = "No results found for '$query'"
+                    empRecyclerView.visibility = View.GONE
+                    tvLoadingData.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
         })
     }
 }
